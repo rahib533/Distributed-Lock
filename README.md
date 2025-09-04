621e45e2171f961480343590e3f5316cbd099d49
Rahib Jafarov <rahib.cefer.98@gmail.com>
2025-09-05 03:13:28 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 🔐 Distributed Lock with Redis & Spring Boot
+
+This project demonstrates how to implement a 
+**distributed lock** in a 
+**Spring Boot** application using **Redis**.
+---
---
+++ src/test/java/az/rahibjafar/distributedlock/DistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package az.rahibjafar.distributedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
